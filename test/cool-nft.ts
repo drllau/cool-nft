9b07613759ea39b5158154be4eae5954322e27a0
Greg <greg@greg.com>
2021-05-20 10:20:04 -0500

    can't transfer again

--- test/cool-nft.ts
+++ test/cool-nft.ts
@@ -14,23 +14,23 @@ const recvAddress = "SPE6KGKEPW6QS8EXT9CAW40AHQ1PNB40K0J554ED"
 const recvKey = "8575f14c6e5d0e0bdaca0a895af7484b39ad596094b11cb5d6ca6ee4e74af89301"
 
 describe("cool-nft contract test suite", () => {
-    let coolClient: Client;
+    let contractClient: Client;
     let provider: Provider;
     before(async () => {
         provider = await ProviderRegistry.createProvider();
-        coolClient = new Client(`${sendAddress}.cool-nft`, "cool-nft", provider);
+        contractClient = new Client(`${sendAddress}.cool-nft`, "cool-nft", provider);
     });
     it("should have a valid syntax", async () => {
-        await coolClient.checkContract();
+        await contractClient.checkContract();
     });
     const getOwner = async () => {
-        const query = coolClient.createQuery({
+        const query = contractClient.createQuery({
             method: {
                 name: "get-owner",
                 args: ['u1']
             }
         });
-        const receipt = await coolClient.submitQuery(query);
+        const receipt = await contractClient.submitQuery(query);
         console.log({
             receipt
         })
@@ -44,14 +44,14 @@ describe("cool-nft contract test suite", () => {
             console.log({
                 sender
             })
-            const tx = coolClient.createTransaction({
+            const tx = contractClient.createTransaction({
                 method: {
                     name: "transfer",
                     args: ['u1', sender, receiver]
                 }
             });
             await tx.sign(sendAddress);
-            const receipt = await coolClient.submitTransaction(tx);
+            const receipt = await contractClient.submitTransaction(tx);
             console.log({
                 receipt
             })
@@ -59,17 +59,17 @@ describe("cool-nft contract test suite", () => {
             return result;
         }
         before(async () => {
-            await coolClient.deployContract();
+            await contractClient.deployContract();
         });
         it("last nft is 1", async () => {
             const getLastTokenId = async () => {
-                const query = coolClient.createQuery({
+                const query = contractClient.createQuery({
                     method: {
                         name: "get-last-token-id",
                         args: []
                     }
                 });
-                const receipt = await coolClient.submitQuery(query);
+                const receipt = await contractClient.submitQuery(query);
                 const result = Result.unwrapUInt(receipt);
                 return result;
             }
@@ -87,14 +87,14 @@ describe("cool-nft contract test suite", () => {
                 console.log({
                     sender
                 })
-                const tx = coolClient.createTransaction({
+                const tx = contractClient.createTransaction({
                     method: {
                         name: "transfer",
                         args: ['u1', sender, receiver]
                     }
                 });
                 await tx.sign(sendAddress);
-                const receipt = await coolClient.submitTransaction(tx);
+                const receipt = await contractClient.submitTransaction(tx);
                 console.log({
                     receipt
                 })
@@ -110,6 +110,72 @@ describe("cool-nft contract test suite", () => {
             const cool3 = await getOwner();
             assert.equal(cool3, `(ok (some ${recvAddress}))`);
         })
+        it("can't transfer again", async () => {
+            const execTransfer = async () => {
+                const sender = `'${sendAddress}`
+                const receiver = `'${recvAddress}`
+                console.log({
+                    sender
+                })
+                const tx = contractClient.createTransaction({
+                    method: {
+                        name: "transfer",
+                        args: ['u1', sender, receiver]
+                    }
+                });
+                await tx.sign(sendAddress);
+                const receipt = await contractClient.submitTransaction(tx);
+                console.log({
+                    receipt
+                })
+                const result = Result.unwrap(receipt);
+                return result;
+            }
+						var failed = false
+						try {
+							const cool8 = await execTransfer();
+							console.log({
+									cool8
+							})
+						} 
+						catch (err) {
+							console.log({
+									err
+							})
+							failed = true
+						}
+						assert.equal(failed, true)
+        })
+        it("can transfer back", async () => {
+            const execTransfer = async () => {
+                const receiver = `'${sendAddress}`
+                const sender = `'${recvAddress}`
+                console.log({
+                    sender
+                })
+                const tx = contractClient.createTransaction({
+                    method: {
+                        name: "transfer",
+                        args: ['u1', sender, receiver]
+                    }
+                });
+                await tx.sign(recvAddress);
+                const receipt = await contractClient.submitTransaction(tx);
+                console.log({
+                    receipt
+                })
+                const result = Result.unwrap(receipt);
+                return result;
+            }
+            const cool1 = await execTransfer();
+            console.log({
+                cool1
+            })
+            assert.equal(cool1, 'Transaction executed and committed. Returned: true\n' +
+                '[NFTEvent(NFTTransferEvent(NFTTransferEventData { asset_identifier: AssetIdentifier { contract_identifier: QualifiedContractIdentifier { issuer: StandardPrincipalData(SP4FZCYV4NQ6BGDT75S75H1W9D0SXMF39F2X21R9), name: ContractName("cool-nft") }, asset_name: ClarityName("cool-nft") }, sender: Standard(StandardPrincipalData(SPE6KGKEPW6QS8EXT9CAW40AHQ1PNB40K0J554ED)), recipient: Standard(StandardPrincipalData(SP4FZCYV4NQ6BGDT75S75H1W9D0SXMF39F2X21R9)), value: UInt(1) }))]');
+            const cool3 = await getOwner();
+            assert.equal(cool3, `(ok (some ${sendAddress}))`);
+        })
         it("can claim", async () => {
             const execClaim = async () => {
                 const sender = `'${sendAddress}`
@@ -117,14 +183,14 @@ describe("cool-nft contract test suite", () => {
                 console.log({
                     sender
                 })
-                const tx = coolClient.createTransaction({
+                const tx = contractClient.createTransaction({
                     method: {
                         name: "claim",
                         args: ['u2']
                     }
                 });
                 await tx.sign(sendAddress);
-                const receipt = await coolClient.submitTransaction(tx);
+                const receipt = await contractClient.submitTransaction(tx);
                 console.log({
                     receipt
                 })
